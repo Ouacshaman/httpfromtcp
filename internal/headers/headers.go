3a3815c61e8688174f3c4b8fc9ad1549fc918613
Shi Hong <shaquanhong@gmail.com>
2025-03-17 01:26:08 -0400

    progress save for header parse

---
+++ internal/headers/headers.go
@@ -0,0 +1,40 @@
+package headers
+
+import (
+	"bytes"
+	"fmt"
+)
+
+type Headers map[string]string
+
+const crlf = "\r\n"
+
+func (h Headers) Parse(data []byte) (n int, done bool, err error) {
+	fmt.Println(string(data))
+
+	// crlf index
+	idx := bytes.Index(data, []byte(crlf))
+	if idx == -1 {
+		return 0, false, nil
+	}
+
+	// if data starts with crlf
+	if idx == 0 {
+		return 0, true, nil
+	}
+
+	afterColon := bytes.SplitAfterN(data[:idx], []byte(":"), 2)
+
+	fieldKey := afterColon[0]
+	if fieldKey[len(fieldKey)-1] == ' ' {
+		return 0, false, fmt.Errorf("FieldName end with Space: %s", string(fieldKey))
+	}
+
+	fieldVal := bytes.TrimSpace(afterColon[1])
+
+	fmt.Println(string(fieldKey), string(fieldVal))
+
+	h[string(fieldKey)] = string(fieldVal)
+
+	return len(data), false, nil
+}
