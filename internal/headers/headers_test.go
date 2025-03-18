da4f924352721c388a02fd97211907bbf9231a9c
Shi Hong <shaquanhong@gmail.com>
2025-03-18 00:02:51 -0400

    added test to header parse function

--- internal/headers/headers_test.go
+++ internal/headers/headers_test.go
@@ -1,9 +1,11 @@
 package headers
 
 import (
+	"bytes"
+	"testing"
+
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
-	"testing"
 )
 
 func NewHeaders() Headers {
@@ -29,4 +31,35 @@ func TestHeaderParse(t *testing.T) {
 	require.Error(t, err)
 	assert.Equal(t, 0, n)
 	assert.False(t, done)
+
+	// Test: Valid Spacing Header
+	headers = NewHeaders()
+	data = []byte("    Host:  localhost:42069  \r\n\r\n")
+	n, done, err = headers.Parse(data)
+	require.NoError(t, err)
+	require.NotNil(t, headers)
+	assert.Equal(t, "localhost:42069", headers["Host"])
+	assert.Equal(t, 30, n)
+	assert.False(t, done)
+}
+
+func TestMultiHeaders(t *testing.T) {
+	headers := NewHeaders()
+	data := []byte("  Host:   localhost:42069 \r\n  User-Agent:   curl/7.81.0\r\n\r\n")
+	list := bytes.SplitAfterN(data, []byte("\r\n"), 3)
+
+	n, done, err := headers.Parse(list[0])
+	require.NoError(t, err)
+	require.NotNil(t, headers)
+	assert.Equal(t, "localhost:42069", headers["Host"])
+	assert.Equal(t, 28, n)
+	assert.False(t, done)
+	n, done, err = headers.Parse(list[1])
+	require.NoError(t, err)
+	require.NotNil(t, headers)
+	assert.Equal(t, "curl/7.81.0", headers["User-Agent"])
+	assert.Equal(t, 29, n)
+	assert.False(t, done)
+	_, done, _ = headers.Parse(list[2])
+	assert.True(t, done)
 }
