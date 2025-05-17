f527a8f9a3c4af7b6b12c8834b014ea7d0aac155
ouacshamanArch <shaquanhong@gmail.com>
2025-05-17 18:33:14 -0400

    added trailer writer in response package

---
+++ internal/response/write_trailer.go
@@ -0,0 +1,19 @@
+package response
+
+import "github.com/Ouacshaman/httpfromtcp/internal/headers"
+
+func (w *Writer) WriteTrailers(h headers.Headers) error {
+	res := ""
+	for k, v := range h {
+		header := fmt.Sprintf("%s: %s\n", k, v)
+		res += header
+	}
+
+	res += "\r\n"
+
+	_, err := w.W.Write([]byte(res))
+	if err != nil {
+		return err
+	}
+	return nil
+}
